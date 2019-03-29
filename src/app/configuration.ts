05c92ccba0500ffa55c4f6411efe1e1540e3cfa4
Subhash Singh <subhash.singh_1982@yahoo.com>
2019-03-29 12:57:54 +0530

    added all files

---
+++ src/app/configuration.ts
@@ -0,0 +1,14 @@
+export const configuration = {
+    header : {
+            headerTitle: 'My Own Thoughts',
+            headerText: 'Do anything whatever you want, But remember you should get some result.',
+            headerButton: 'Do Something',
+            headerLink: '/Home'
+    },
+    testimonial:{
+        TestiTitle: 'My Own Thoughts',
+        headerText: 'Do anything whatever you want, But remember you should get some result.',
+        headerButton: 'Do Something',
+        headerLink: '/Home'
+},
+};
